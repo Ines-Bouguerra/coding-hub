e980b8d58d9f5649af0c46f3bd7a6154e968e21c
Ines Bouguerra <ines.bouguerra.22@gmail.com>
2023-01-10 12:18:35 +0100

    Add JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,26 +1,23 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
-
     // incorrect email
     if (err.message === 'incorrect email') {
         errors.email = 'That email is not registered';
     }
-
     // incorrect password
     if (err.message === 'incorrect password') {
         errors.password = 'That password is incorrect';
     }
-
     // duplicate email error
     if (err.code === 11000) {
         errors.email = 'that email is already registered';
         return errors;
     }
-
     // validation errors
     if (err.message.includes('user validation failed')) {
         // console.log(err);
@@ -30,15 +27,22 @@ const handleErrors = (err) => {
             errors[properties.path] = properties.message;
         });
     }
-
     return errors;
 }
 
+const createToken = (id) => {
+    return jwt.sign({
+        id
+    }, process.env.JWT_SECRET, {
+        expiresIn: 3 * 24 * 60 * 60
+    });
+}
 module.exports.register = async (req, res) => {
-    const { email, password } = req.body;
-
+    const { email, password } = req.body;  // destructure req.body
     try {
         const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 3 * 24 * 60 * 60 * 1000 });
         res.status(201).json(user);
     }
     catch (err) {
@@ -49,7 +53,18 @@ module.exports.register = async (req, res) => {
 
 module.exports.login = async (req, res) => {
     const { email, password } = req.body;  // destructure req.body
+    try {
+      const user = await User.login(email, password);
+      const token = createToken(user._id);
 
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+      });
+      res.status(200).json({ user: user._id });
+    } catch (err) {
+        res.status(400).send({ errors: err.message });
+    }
     console.log(email, password);
     res.send('user login');
 }
